1287f4bc02d5fc68b7047f1859d6e860bdecfb48
Pedro Henrique Lima <pedroconicz@gmail.com>
2026-06-23 09:19:55 -0300

    fix: corrige sintaxe do UserModel e nome do diretório

--- VertrauDesafioTecnico/Data/AppDbContext.cs
+++ VertrauDesafioTecnico/Data/AppDbContext.cs
@@ -10,5 +10,5 @@ public class AppDbContext : DbContext
     {
     }
 
-    public DbSet<User> Users { get; set; }
+    public DbSet<UserModel> Users { get; set; }
 }
---
+++ VertrauDesafioTecnico/Model/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VertrauDesafioTecnico.Entity;
+
+public class UserModel
+{
+    public long Id { get; set; }
+    
+    [Required]
+    public string Nome { get; set; }
+    
+    [Required]
+    public string Sobrenome { get; set; }
+    
+    [Required]
+    public string Email { get; set; }
+    
+    [Required]
+    public Genero Genero { get; set; }
+    
+    public DateTime? DataNascimento { get; set; }
+}
