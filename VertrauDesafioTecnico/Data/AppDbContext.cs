9cf25eb69a0e29fe4bc2fcb222d6de2cfbc3c303
Pedro Henrique Lima <pedroconicz@gmail.com>
2026-06-24 08:38:23 -0300

    feat: add unique constraint para email do usuário

--- VertrauDesafioTecnico/Data/AppDbContext.cs
+++ VertrauDesafioTecnico/Data/AppDbContext.cs
@@ -11,4 +11,12 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<UserModel> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<UserModel>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+    }
 }
