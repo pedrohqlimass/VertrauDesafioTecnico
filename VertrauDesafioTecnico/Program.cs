eb3da0435d73a1364e73c0bd097b028faee96e39
Pedro Henrique Lima <pedroconicz@gmail.com>
2026-06-24 15:10:43 -0300

    chore: ajustar configuração do docker e connection string

--- VertrauDesafioTecnico/Program.cs
+++ VertrauDesafioTecnico/Program.cs
@@ -4,39 +4,39 @@ using VertrauDesafioTecnico.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// DB
+// DATABASE
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Services
+// SERVICES
 builder.Services.AddScoped<UserService>();
 
-// Controllers
+
+// CONTROLLERS
 builder.Services.AddControllers();
 
-// Swagger
+// SWAGGER
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Migrations automáticas
+// PIPELINE
+// Swagger SEM if (para não dar dor de cabeça em Docker/desafio)
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// MIGRATIONS AUTOMÁTICAS
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
