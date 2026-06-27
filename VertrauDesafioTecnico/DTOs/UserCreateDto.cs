1777e0a91743e87beccefd224fa5097ea64e861d
Pedro Henrique Lima <pedroconicz@gmail.com>
2026-06-27 09:51:09 -0300

    feat: implementa validações com DataAnnotations no UserCreateDto

--- VertrauDesafioTecnico/DTOs/UserCreateDto.cs
+++ VertrauDesafioTecnico/DTOs/UserCreateDto.cs
@@ -1,4 +1,5 @@
-﻿using VertrauDesafioTecnico.Model;
+﻿using System.ComponentModel.DataAnnotations;
+using VertrauDesafioTecnico.Model;
 
 namespace VertrauDesafioTecnico.DTOs;
 
@@ -6,12 +7,19 @@ public class UserCreateDto
 {
     public long Id { get; set; }
     
+    [Required]
+    [MaxLength(100)]
     public string Nome { get; set; }
     
+    [Required]
+    [MaxLength(100)]
     public string Sobrenome { get; set; }
     
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
     
+    [Required]
     public Genero Genero { get; set; }
     
     public DateTime? DataNascimento { get; set; }
